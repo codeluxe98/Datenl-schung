21365c6b38f3a478c6cd7ae9dc3f3e6a70691642
codeluxe98 <info@d-eubel.de>
2025-07-11 22:29:00 +0200

    Remove unused request import

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from flask_wtf import FlaskForm
